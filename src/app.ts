ca279c5d0addf67ed3b39fd0d107b8bba1241b2f
Roman <fniflheim@gmail.com>
2022-12-23 11:39:16 +0200

    added environment variable

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import {Telegraf, Context} from 'telegraf'
 import { Update } from 'telegraf/typings/core/types/typegram';
-import { BotController } from './Controllers/botController'
+import { BotController } from './Controllers/botController';
+import dotenv from 'dotenv';
 
-const bot = new Telegraf<Context<Update>>(process.env.TELEGRAM_BOT_TOKEN);
+dotenv.config();
+
+const bot = new Telegraf<Context<Update>>(process.env.TELEGRAM_BOT_TOKEN as string);
 
 const botController = new BotController(bot);
 botController.run();
