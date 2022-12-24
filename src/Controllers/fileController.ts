2bdb01d8c670bfecf26976b5087952c5350fb69c
darkxp <andrejveselyj90@gmail.com>
2022-12-24 18:55:24 +0200

    added async to deleteFile() method

--- src/Controllers/fileController.ts
+++ src/Controllers/fileController.ts
@@ -4,28 +4,33 @@ import https from 'https'
 export class FileController {
     static async downloadFile(url: URL, outputFilename: string): Promise<void> {
         return new Promise((resolve, reject) => {
-            const file = fs.createWriteStream("temp\\" + outputFilename);
+            const file = fs.createWriteStream("build/temp/" + outputFilename);
 
-                https.get(url, (response) => {
-                    response.pipe(file);
+            https.get(url, (response) => {
+                response.pipe(file);
 
-                    file.on("finish", () => {
-                        resolve(file.close());
-                    });
+                file.on("finish", () => {
+                    resolve(file.close());
+                });
 
-                    file.on("error", (err) => {
-                        file.close();
-                        reject(err);
-                    });
+                file.on("error", (err) => {
+                    file.close();
+                    reject(err);
                 });
             });
+        });
     }
 
-    static deleteFile(filename: string): void {
-        fs.unlink(filename, (err) => {
-            if (err) {
-                throw err;
-            }
+    static async deleteFile(filename: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            fs.unlink(filename, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
         });
     }
 }
