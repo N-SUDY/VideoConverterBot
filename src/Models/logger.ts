d75b5b00457e301258d7c2692dfec366f77eac11
darkxp <andrejveselyj90@gmail.com>
2022-12-29 20:28:05 +0200

    added logger

---
+++ src/Models/logger.ts
@@ -0,0 +1,34 @@
+import { Logger } from "ts-log";
+import fs from 'fs'
+
+export class FileLogger implements Logger {
+    private readonly fd: number;
+
+    constructor(filename: string) {
+        this.fd = fs.openSync(filename, "a");
+    }
+
+    private append(type: string, message: string) {
+        fs.writeSync(this.fd, `${new Date().toISOString()} ${type} ${message}\n`);
+    }
+
+    public trace(message?: any, ...optionalParams: any[]): void {
+        this.append("TRACE", `${message} ${JSON.stringify(optionalParams)}`);
+    }
+
+    public debug(message?: any, ...optionalParams: any[]): void {
+        this.append("DEBUG", `${message} ${JSON.stringify(optionalParams)}`);
+    }
+
+    public info(message?: any, ...optionalParams: any[]): void {
+        this.append("INFO ", `${message} ${JSON.stringify(optionalParams)}`);
+    }
+
+    public warn(message?: any, ...optionalParams: any[]): void {
+        this.append("WARN ", `${message} ${JSON.stringify(optionalParams)}`);
+    }
+
+    public error(message?: any, ...optionalParams: any[]): void {
+        this.append("ERROR", `${message} ${JSON.stringify(optionalParams)}`);
+    }
+}
