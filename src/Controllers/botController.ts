6dcd3453487b160aad751a99be3c421e15da4bc2
darkxp <andrejveselyj90@gmail.com>
2023-01-11 09:05:11 +0200

    added try/catch blocks and new features

--- src/Controllers/botController.ts
+++ src/Controllers/botController.ts
@@ -1,4 +1,5 @@
 import { extensions } from "../Models/extensions";
+import { extensionsKeyboard } from "../Models/keyboards";
 import { FileController } from "./fileController";
 import { FFmpegController } from "./ffmpegController";
 import { message } from "telegraf/filters"
@@ -13,108 +14,134 @@ export class BotController {
     private readonly bot: Telegraf<Context<Update>>;
     private readonly ffMpegController: FFmpegController;
     private readonly video: Video;
-    private readonly keyboard: {text: string, callback_data: string}[][];
-
+    
     constructor(bot: Telegraf<Context<Update>>) {
         this.logger = new FileLogger("log.txt");
         this.bot = bot;
         this.ffMpegController = new FFmpegController(process.env.FFMPEG_PATH as string);
-        this.video = new Video(); 
-
-        this.keyboard = [
-            [{ text: "MP4", callback_data: ".mp4" }],
-            [{ text: "AVI", callback_data: ".avi" }],
-            [{ text: "MOV", callback_data: ".mov"}],
-            [{ text: "WMV", callback_data: ".wmv"}],
-            [{ text: "MKV", callback_data: ".mkv"}]
-        ];
+        this.video = new Video();
     }
 
+    //This method checks video extension and file size. 
+    //In the good case, method gets and sets url to video and send message with keyboard that contains extensions.
+    //In the worst case, method send message to user about certain problem.
+    private async validateData(context: Context<Update>, extensions: string[], extension: string, fileSize: number): Promise<void> {
+        if (FileController.validateFileExtension(extensions, extension)) {
+            if (FileController.validateFileSize(fileSize)) {
+                await context.telegram.getFileLink(this.video.name)
+                    .then((url) => this.video.url = url);
+
+                await context.telegram.sendMessage(context.message!.chat.id,
+                    "Choose extension",
+                    {
+                        parse_mode: 'HTML',
+                        reply_markup: { inline_keyboard: extensionsKeyboard },
+                        reply_to_message_id: context.message!.message_id
+                    });
+            }
+            else {
+                await context.telegram.sendMessage(context.message!.chat.id,
+                    "❌Too big file!❌",
+                    {
+                        reply_to_message_id: context.message!.message_id
+                    });
+            }
+        }
+        else {
+            await context.telegram.sendMessage(context.message!.chat.id,
+                "❌The file extension does not match the requirement!❌",
+                {
+                    reply_to_message_id: context.message!.message_id
+                });
+        }
+    }
+    
+    //This method checks on empty strings. 
+    //If one string is empty, then throw Error.
+    //Else the next code will be executed
     private async runConverter(): Promise<void> {
-        await FileController.downloadFile(this.video.url!, `${this.video.id}${this.video.extension}`);
-        await this.ffMpegController.Convert(`build/temp/${this.video.id}${this.video.extension}`, `build/temp/${this.video.id}${this.video.futureExtension}`);
+        if (!FileController.validateFileName(this.video.name) || !FileController.validateFileExtension(extensions, this.video.extension)) {
+            throw new Error("Empty video name or extension!");
+        }
+
+        await FileController.downloadFile(this.video.url!, `${this.video.name}${this.video.extension}`);
+        await this.ffMpegController.Convert(`build/temp/${this.video.name}${this.video.extension}`, `build/temp/${this.video.name}${this.video.futureExtension}`);
     }
 
+    //This method contains handlers for telegram bot.
     public run(): void {
         this.bot.start(async (context) => {
-            await context.telegram.sendMessage(context.message.chat.id,
-                `Hi, <b>${context.from.first_name}</b>!` +
-                `\nMy name is ${context.botInfo.first_name}, i can convert you video in required format.` +
-                `\nJust send video or document.`, 
-                { 
-                    parse_mode: 'HTML' 
+            await context.telegram.sendPhoto(context.message.chat.id,
+                {
+                    source: process.env.START_PHOTO as string
+                },
+                {
+                    caption: `Hi, <b>${context.from.first_name}</b>!` +
+                    `\nMy name is ${context.botInfo.first_name}, I can convert you video in required format.` +
+                    `\nJust send video or document.`,
+                    parse_mode: 'HTML'
+                })
+                .catch((error) => {
+                    this.logger.error(error, error.stack);
                 });
         });
 
         this.bot.help(async (context) => {
-            await context.telegram.sendMessage(context.message.chat.id,
-                `<b>${context.botInfo.first_name}</b> - this is a bot, that can convert your video from certain extension to other.` +
-                `\n\nAt the moment, the bot can convert to the following extensions: <b>\nmp4\navi\nmov\nmkv\nwmv</b>` +
-                `\n\nYou have 2 option to send the video: \n<b>1.Send as video</b>\n<b>2.Send as document</b>`,
-                { 
-                    parse_mode: 'HTML' 
+            await context.telegram.sendPhoto(context.message.chat.id, 
+                {
+                    source: process.env.HELP_PHOTO as string
+                },
+                {
+                    caption: `<b>${context.botInfo.first_name}</b> - this is a bot, that can convert your video from certain extension to other.` +
+                    `\n\nAt the moment, the bot can convert to the following extensions: <b>\nmp4\navi\nmov\nmkv\nwmv</b>` +
+                    `\n\nYou have 2 option to send the video: \n<b>1.Send as video</b>\n<b>2.Send as document</b>`,
+                    parse_mode: 'HTML'
+                })
+                .catch((error) => {
+                    this.logger.error(error, error.stack);
                 });
         });
 
         this.bot.on(message('video'), async (context) => {
-            if (context.message.video.file_size! <= 20971520) {
-                this.video.id = context.message.video.file_id;
-                this.video.name = context.message.video.file_name!;
-                this.video.extension = path.extname(context.message.video.file_name!);
+            try {
+                if (context.message.forward_date) {
+                    await context.telegram.sendMessage(context.message!.chat.id,
+                        "❌I do not process the forwarded message! Upload your video via computer or phone!❌",
+                        {
+                            reply_to_message_id: context.message!.message_id
+                        });
 
-                await context.telegram.getFileLink(this.video.id)
-                .then((url) => this.video.url = url);
+                    return;
+                }
 
-                await context.telegram.sendMessage(context.message.chat.id,
-                    "Choose extension",
-                    {
-                        parse_mode: 'HTML',
-                        reply_markup: { inline_keyboard: this.keyboard },
-                        reply_to_message_id: context.message.message_id
-                    });
-            }
-            else {
-                await context.telegram.sendMessage(context.message.chat.id, 
-                    "❌Too big file!❌", 
-                    {
-                        reply_to_message_id: context.message.message_id
-                    });
+                this.video.name = context.message.video.file_id;
+                this.video.extension = path.extname(context.message.video.file_name!); 
+
+                this.validateData(context, extensions, this.video.extension, context.message.video.file_size!);
+            } catch (error) {
+                this.logger.error(error);
             }
         });
 
         this.bot.on(message('document'), async (context) => {
-            const extension = path.extname(context.message.document.file_name!);
-
-            if (extensions.includes(extension)) {
-                if (context.message.document.file_size! <= 20971520) {
-                    this.video.id = context.message.document.file_id;
-                    this.video.name = context.message.document.file_name!;
-                    this.video.extension = extension;
-    
-                    await context.telegram.getFileLink(this.video.id)
-                    .then((url) => this.video.url = url);
-    
-                    await context.telegram.sendMessage(context.message.chat.id, "Choose extension", 
-                    { 
-                        parse_mode: 'HTML', 
-                        reply_markup: { inline_keyboard: this.keyboard }, 
-                        reply_to_message_id: context.message.message_id 
-                    });
-                }
-                else {
-                    await context.telegram.sendMessage(context.message.chat.id, 
-                        "❌Too big file!❌", 
+            try {
+                if (context.message.forward_date) {
+                    await context.telegram.sendMessage(context.message!.chat.id,
+                        "❌I do not process the forwarded message! Upload your video via computer or phone!❌",
                         {
-                            reply_to_message_id: context.message.message_id
+                            reply_to_message_id: context.message!.message_id
                         });
+
+                    return;
                 }
+
+                this.video.name = context.message.document.file_id;
+                this.video.extension = path.extname(context.message.document.file_name!);
+                
+                this.validateData(context, extensions, this.video.extension, context.message.document.file_size!)
             }
-            else {
-                await context.telegram.sendMessage(context.message.chat.id, 
-                    "❌The file extension does not match the requirement!❌", 
-                    {
-                        reply_to_message_id: context.message.message_id
-                    });
+            catch (error) {
+                this.logger.error(error);
             }
         });
 
@@ -122,50 +149,50 @@ export class BotController {
             this.video.futureExtension = context.match[0];
 
             if (this.video.extension == this.video.futureExtension) {
-                await context.telegram.editMessageText(context.callbackQuery.message!.chat.id, 
+                await context.telegram.editMessageText(context.callbackQuery.message!.chat.id,
                     context.callbackQuery.message!.message_id,
-                    context.callbackQuery.inline_message_id, 
-                    "❌Future extension is equal to the current one!❌", 
+                    context.callbackQuery.inline_message_id,
+                    "❌Future extension is equal to the current one!❌",
                     {
                         parse_mode: 'HTML'
                     });
 
-                    return;
+                return;
             }
 
-            await context.telegram.editMessageText(context.callbackQuery.message!.chat.id, 
+            await context.telegram.editMessageText(context.callbackQuery.message!.chat.id,
                 context.callbackQuery.message!.message_id,
-                context.callbackQuery.inline_message_id, 
+                context.callbackQuery.inline_message_id,
                 "Processing...");
 
             await this.runConverter()
-            .then(async () => {
-                await context.telegram.editMessageText(context.callbackQuery.message!.chat.id, 
-                    context.callbackQuery.message!.message_id,
-                    context.callbackQuery.inline_message_id, 
-                    "✅Done!✅");
-    
-                await this.bot.telegram.sendVideo(context.callbackQuery.message?.chat.id!,
-                    {
-                        source: `build/temp/${this.video.id}${this.video.futureExtension}`,
-                    },
-                    {
-                        caption: `${this.video.extension} ➡️ ${this.video.futureExtension}`
-                    });
-    
-                await FileController.deleteFile(`build/temp/${this.video.id}${this.video.extension}`);
-                await FileController.deleteFile(`build/temp/${this.video.id}${this.video.futureExtension}`);
-            })
-            .catch(async (err) => {
-                await context.telegram.editMessageText(context.callbackQuery.message!.chat.id, 
-                    context.callbackQuery.message!.message_id,
-                    context.callbackQuery.inline_message_id, 
-                    "❌Something went wrong, try again!❌");
+                .then(async () => {
+                    await context.telegram.editMessageText(context.callbackQuery.message!.chat.id,
+                        context.callbackQuery.message!.message_id,
+                        context.callbackQuery.inline_message_id,
+                        "✅Done!✅");
 
-                await FileController.deleteFile(`build/temp/${this.video.id}${this.video.extension}`);
+                    await this.bot.telegram.sendVideo(context.callbackQuery.message?.chat.id!,
+                        {
+                            source: `build/temp/${this.video.name}${this.video.futureExtension}`,
+                        },
+                        {
+                            caption: `${this.video.extension} ➡️ ${this.video.futureExtension}`
+                        });
 
-                this.logger.error(err, err.stack);
-            });
+                    await FileController.deleteFile(`build/temp/${this.video.name}${this.video.extension}`);
+                    await FileController.deleteFile(`build/temp/${this.video.name}${this.video.futureExtension}`);
+                })
+                .catch(async (err) => {
+                    await context.telegram.editMessageText(context.callbackQuery.message!.chat.id,
+                        context.callbackQuery.message!.message_id,
+                        context.callbackQuery.inline_message_id,
+                        "❌Something went wrong, try again!❌");
+
+                    await FileController.deleteFile(`build/temp/${this.video.name}${this.video.extension}`);
+
+                    this.logger.error(err, err.stack);
+                });
         });
     }
 }
